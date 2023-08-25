382abd2306332b4f4b23432dbcd7b30ac014f118
fenayati <fenayati302@gmail.com>
2023-08-25 18:02:52 -0400

    add types

---
+++ types/index.ts
@@ -0,0 +1,8 @@
+import MouseEventHandler from 'react'
+
+export interface CustomButtomProps {
+  title: string;
+  containerStyle: string;
+  handleClick: 
+  MouseEventHandler<HTMLButtonElement>;
+}
